d233d8e5d91e09be7067a4f41396290835c803f4
Amen <jamen.jamne30@gmail.com>
2023-05-04 09:18:52 +0300

    working on super admin -not completed-

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,8 +2,9 @@ import express from "express";
 const router = express.Router();
 import adminController from "../controllers/adminController.js";
 import authorization from "../middleware/adminAuth.js";
+import test from "../middleware/superAdminAuth.js";
 
-router.get("/", authorization, adminController.getAllAdmins);
+router.get("/", test, adminController.getAllAdmins);
 router.get("/:id", authorization, adminController.getAdmin);
 router.put("/:id", authorization, adminController.editAdmin);
 router.delete("/:id", authorization, adminController.deleteAdmin);
