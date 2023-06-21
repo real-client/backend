33e8c5a5db8be131750d36deee3dd4c6e4210297
Marwa <marwa.al.shahal@gmail.com>
2023-06-21 16:46:17 +0300

    volunteer model

--- models/volunteersModels.js
+++ models/volunteersModels.js
@@ -1,24 +1,60 @@
-import mongoose from 'mongoose';
+import { Schema, model } from "mongoose";import { Schema, model } from "mongoose";
 
-const volunteersSchema = mongoose.Schema({
+const volunteerSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    title: [
+      {
+        type: String,
+      },
+    ],
 
-    firstName: { type: String, required: true },
+    faculty: {
+      type: String,
+      required: true,
+    },
+    branch: {
+      type: String,
+      required: true,
+    },
+    starting_date: {
+      type: Date,
+      required: true,
+    },
+    ending_date: {
+      type: Date,
+      default: null,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    contact: {
+      email: { type: String },
+      phone: { type: String },
+      socialMedia: {
+        twitter: { type: String },
+        instagram: { type: String },
+        facebook: { type: String },
+        linkedin: { type: String },
+      },
+    },
+  },
+  {
+    collection: "Volunteers",
+    timestamps: true,
+  }
+);
 
-    lastName: { type: String, required: true },
-    
-    image: { type: String },
-
-    title: { type: String, required: true },
-
-    faculty: { type: String, required: true },
-
-    branch: { type: String, required: true },
-
-    startingDate: { type: Date, required: true },
-
-    endingDate: { type: Date, required: true }
-
-
-})
-
-export default mongoose.model('Volunteer', volunteersSchema);
+const Volunteer = model("Volunteer", volunteerSchema);
+export default Volunteer;
