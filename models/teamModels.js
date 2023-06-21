f240ee1a963f79802e790cb203b02b4f6f3cec1b
Marwa <marwa.al.shahal@gmail.com>
2023-06-21 16:45:44 +0300

    team model

--- models/teamModels.js
+++ models/teamModels.js
@@ -1,26 +1,58 @@
-import mongoose from 'mongoose';
+import { Schema, model } from "mongoose";
 
-const teamSchema = mongoose.Schema({
+const teamSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    title: [
+      {
+        type: String,
+      },
+    ],
 
-    firstName: { type: String},
+    role: {
+      type: String,
+      enum: ["director", "co-founder", "main founder", "staff"],
+      required: true,
+    },
 
-    lastName: { type: String},
+    starting_date: {
+      type: Date,
+      required: true,
+    },
+    ending_date: {
+      type: Date,
+      default: null,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    contact: {
+      email: { type: String },
+      phone: { type: String },
+      socialMedia: {
+        twitter: { type: String },
+        instagram: { type: String },
+        facebook: { type: String },
+        linkedin: { type: String },
+      },
+    },
+  },
+  {
+    collection: "Teams",
+    timestamps: true,
+  }
+);
 
-    image: { type: String },
-
-    title: { type: String },
-
-    email: { type: String }, 
-
-    linkedin: { type: String },
-
-    role: { type: String }
-
-})
-
-export default mongoose.model('Team', teamSchema);
-
-
-
-
-
+const Team = model("Team", teamSchema);
+export default Team;
