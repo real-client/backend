0861b28d14b1f83218c345117848cfa2bd93b997
Marwa <marwa.al.shahal@gmail.com>
2023-06-21 16:45:36 +0300

    partner model

--- models/partnerModel.js
+++ models/partnerModel.js
@@ -1,22 +1,27 @@
-import mongoose from "mongoose";
 const { Schema, model } = mongoose;
+
 const partnerSchema = new Schema(
   {
     name: {
       type: String,
       required: true,
-      unique: [true, "name already exist"],
+      unique: [true, "Name already exists"],
     },
     logo: {
       type: String,
       required: true,
     },
+    website_link: {
+      type: String,
+      required: true,
+    },
   },
-  { timestamps: true },
   {
-    collection: "partner",
+    collection: "Partners",
+    timestamps: true,
   }
 );
 
-const partnerModel = model("partner", partnerSchema);
-export default partnerModel;
+const Partner = model("Partner", partnerSchema);
+
+export default Partner;
